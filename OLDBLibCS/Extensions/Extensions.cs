bb4ae826d90dc400dd7898aabcf98ea3e73bd5c6
kaolick <kaolick@gmail.com>
2019-06-18 11:00:48 +0200

    Add extensions

---
+++ OLDBLibCS/Extensions/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OLDBLibCS.Extensions
+{
+    public static class Extensions
+    {
+        public static string PrettifyJson(this string json)
+        {
+            return JToken.Parse(json).ToString(Formatting.Indented);
+        }
+    }
+}
